fa80799e0e109cd00bc4c7bd15b460c3272eae46
Cameron Jordan <cjordan@cameronism.com>
2012-02-08 01:09:41 -0800

    Added LICENSE

--- Cameronism.Csv.Tests/Verify.cs
+++ Cameronism.Csv.Tests/Verify.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/* Cameronism.Csv
+ * Copyright © 2012 Cameronism.com.  All Rights Reserved.
+ * 
+ * Apache License 2.0 - http://www.apache.org/licenses/LICENSE-2.0
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
