599a2528a38490c4216de6d3433d3c73a72e7dc0
Cameron Jordan <cjordan@cameronism.com>
2012-04-22 01:23:58 -0700

    Set assembly metadata

--- Cameronism.CSV/Properties/AssemblyInfo.cs
+++ Cameronism.CSV/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Cameronism.CSV")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Cameronism.Csv")]
+[assembly: AssemblyDescription("CSV serializer based on expression trees")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Cameronism.CSV")]
+[assembly: AssemblyCompany("Cameron Jordan")]
+[assembly: AssemblyProduct("Cameronism.Csv")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
